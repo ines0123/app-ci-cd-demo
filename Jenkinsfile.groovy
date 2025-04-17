f633d80616493e5cdf43d8c120bfb989451df42a
ines0123 <94559280+ines0123@users.noreply.github.com>
2025-04-17 09:41:16 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,8 @@
 pipeline {
     agent any
     environment {
-        DOCKER_IMAGE = 'ines0123/ci-cd-demo-app' 
+        DOCKER_IMAGE = 'ci-cd-demo-app' 
+        DOCKER_TAG = "${env.BUILD_NUMBER}"
         HELM_CHART_PATH = './mon-app'  
     }
     stages {
@@ -27,7 +28,20 @@ pipeline {
                 script {
                     withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
                         sh "echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin"
+                        sh "docker tag ${DOCKER_IMAGE}:${DOCKER_TAG} ${DOCKER_IMAGE}:${DOCKER_TAG}"
                         sh "docker push ${DOCKER_IMAGE}:${BUILD_NUMBER}"
+                    }
+                }
+            }
+        }
+        stage('Test kubeconfig') {
+            steps {
+                script {
+                    withCredentials([file(credentialsId: 'kubeconfig-id', variable: 'KUBECONFIG')]) {
+                        sh """
+                            export KUBECONFIG=\${KUBECONFIG}
+                            kubectl get pods
+                        """
                     }
                 }
             }
@@ -37,11 +51,23 @@ pipeline {
                 script {
                     withCredentials([file(credentialsId: 'kubeconfig-id', variable: 'KUBECONFIG')]) {
                         sh """
-                            export KUBECONFIG=$KUBECONFIG
+                            export KUBECONFIG=\${KUBECONFIG}
                             helm upgrade --install mon-app ${HELM_CHART_PATH} \
                                 --set image.repository=${DOCKER_IMAGE} \
                                 --set image.tag=${BUILD_NUMBER}
                         """
+                    }
+                }
+            }
+        }
+        stage('Check cluster') {
+            steps {
+                script {
+                    withCredentials([file(credentialsId: 'kubeconfig-id', variable: 'KUBECONFIG')]) {
+                        sh '''
+                            export KUBECONFIG=$KUBECONFIG
+                            kubectl get pods -A
+                        '''
                     }
                 }
             }
